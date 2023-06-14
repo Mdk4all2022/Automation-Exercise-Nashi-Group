0525304b0169f4684e171fe593e61ab20589d49e
adamar <aysun114@hotmail.com>
2023-06-14 11:17:39 -0500

    VytrackLoginPage class is added

---
+++ src/test/java/com/cydeo/tests/week_01/morning/VytrackLoginTest.java
@@ -0,0 +1,15 @@
+package com.cydeo.tests.week_01.morning;
+
+import com.google.common.base.Verify;
+
+public class VytrackLoginTest {
+
+    //TC002 As a user I should be able to see the login page
+    // 1- Setup the "browser driver"
+    // 2- Go to "https://vytrack.com"
+    // 3- Click Login label
+    // 3- Verify the title contains "Login"
+
+
+    @Test
+}
