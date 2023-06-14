800da1d72a9dad44d8e5839e4b5caef779bf5261
adamar <aysun114@hotmail.com>
2023-06-14 11:40:06 -0500

    VytrackLoginPage class is added

--- src/test/java/com/cydeo/tests/week_01/morning/VytrackLoginTest.java
+++ src/test/java/com/cydeo/tests/week_01/morning/VytrackLoginTest.java
@@ -1,6 +1,14 @@
 package com.cydeo.tests.week_01.morning;
 
 import com.google.common.base.Verify;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class VytrackLoginTest {
 
@@ -12,4 +20,24 @@ public class VytrackLoginTest {
 
 
     @Test
+    public void vytrack_title_verification_test(){
+        // 1- Setup the "browser driver"
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        // 2- Go to "https://vytrack.com"
+        driver.get("https://vytrack.com");
+
+        // 3- Click Login label
+        WebElement loginLink = driver.findElement(By.xpath("(//a[.='LOGIN'])[1]"));
+        loginLink.click();
+
+
+        // 3- Verify the title contains "Login"
+
+    }
+
+
 }
