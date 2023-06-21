79fcf66ee2cd906b2c913b419495b9c88e73b827
adamar <aysun114@hotmail.com>
2023-06-21 10:28:46 -0500

    utilities classes added

---
+++ src/test/java/com/cydeo/tests/week_02/CheckboxPractice.java
@@ -0,0 +1,37 @@
+package com.cydeo.tests.week_02;
+
+import com.cydeo.tests.base.TestBase;
+import org.testng.annotations.Test;
+
+public class CheckboxPractice extends TestBase {
+
+    //TC001 As a user I should be able to click checkboxes
+    //
+    //  1-open a chrome browser
+    //  2-goto https://practice.cydeo.com/
+    //  3-click Checkboxes
+    //  4-verify title contains Checkboxes
+    //  5-click Checkbox 1
+    //  6-verify the Checkbox 1 is checked
+    //  7-click Checkbox 2
+    //  8-verify the Checkbox 2 is checked
+
+    @Test
+    public void checkbox_text(){
+        //  2-goto https://practice.cydeo.com/
+
+
+        //  3-click Checkboxes
+
+
+        //  4-verify title contains Checkboxes
+
+
+        //  5-click Checkbox 1
+        //  6-verify the Checkbox 1 is checked
+        //  7-click Checkbox 2
+        //  8-verify the Checkbox 2 is checked
+
+    }
+
+}
