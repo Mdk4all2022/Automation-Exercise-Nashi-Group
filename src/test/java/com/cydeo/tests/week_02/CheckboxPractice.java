1d352fc8bb52439432332a9cc35aba68542afe2e
adamar <aysun114@hotmail.com>
2023-06-21 11:01:44 -0500

    CheckboxPractice class added

--- src/test/java/com/cydeo/tests/week_02/CheckboxPractice.java
+++ src/test/java/com/cydeo/tests/week_02/CheckboxPractice.java
@@ -1,6 +1,10 @@
 package com.cydeo.tests.week_02;
 
 import com.cydeo.tests.base.TestBase;
+import com.cydeo.utilities.BrowserUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class CheckboxPractice extends TestBase {
@@ -19,18 +23,41 @@ public class CheckboxPractice extends TestBase {
     @Test
     public void checkbox_text(){
         //  2-goto https://practice.cydeo.com/
-
+        driver.get("https://practice.cydeo.com/");
 
         //  3-click Checkboxes
+        // Since I will not need Checkbox webelement in next coming lines, I did not store in WebElement
+        driver.findElement(By.linkText("Checkboxes")).click();
 
+        //  4-verify title equals Checkboxes
+       // Assert.assertTrue(driver.getTitle().equals("Checkboxes"));
+        Assert.assertEquals(driver.getTitle(),"Checkboxes");
 
-        //  4-verify title contains Checkboxes
+//        String actualTitle = driver.getTitle();
+//        String expectedTitle = "Checkboxes";
+//
+//       // Assert.assertTrue(actualTitle.equals(expectedTitle));
+//        Assert.assertEquals(actualTitle,expectedTitle);
+
 
 
         //  5-click Checkbox 1
-        //  6-verify the Checkbox 1 is checked
+        WebElement checkbox1 = driver.findElement(By.id("box1"));
+        checkbox1.click();
+
+        BrowserUtils.sleep(3);
+
+        //  6-verify the Checkbox 1 is selected
+        Assert.assertTrue(checkbox1.isSelected(),"checkbox1 verification failed!");
+
+
         //  7-click Checkbox 2
-        //  8-verify the Checkbox 2 is checked
+        WebElement checkbox2 = driver.findElement(By.id("box2"));
+        checkbox2.click();
+
+        //  8-verify the Checkbox 2 is not selected
+        Assert.assertTrue(!checkbox2.isSelected(),"checkbox2 verification failed!");
+
 
     }
 
