87dd4fc495fe6c32d532b586bfee1a38d89b9c1b
adamar <aysun114@hotmail.com>
2023-06-26 12:31:14 -0500

    VytrackTest class shared

--- src/test/java/com/cydeo/tests/avengers/week2/VytrackTest.java
+++ src/test/java/com/cydeo/tests/avengers/week2/VytrackTest.java
@@ -10,6 +10,8 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class VytrackTest {
@@ -37,13 +39,24 @@ public class VytrackTest {
 //
 //  1-open a chrome browser
 //  2-goto https://vytrack.com/
-//  3-verify Home, About us, Our Approach, Product and Services, Contact and LOGIN links are displayed
+//  3-verify Home, About us, Our Approach, Products and Services, Contact and LOGIN links are displayed
         List<WebElement> allTopLinks = driver.findElements(By.xpath("//ul[@id='top-menu']/li/a"));
 
+//        for (WebElement eachLink : allTopLinks) {
+//            System.out.println(eachLink.getText());
+//            Assert.assertTrue(eachLink.isDisplayed());
+//        }
+
+        List<String> actualTopLinks = new ArrayList<>();
+
         for (WebElement eachLink : allTopLinks) {
-            System.out.println(eachLink.getText());
-            Assert.assertTrue(eachLink.isDisplayed());
+            String eachLinkText = eachLink.getText();
+            actualTopLinks.add(eachLinkText);
         }
+
+        List<String> expectedTopLinks = new ArrayList<>(Arrays.asList("Home", "About us", "Our Approach", "Products and Services", "Contact", "LOGIN"));
+
+        Assert.assertEquals(actualTopLinks,expectedTopLinks,"Top links verification failed!");
 
     }
 }
