0d4b2582e22b1cb523a46ad77a357b4114f60f2d
adamar <aysun114@hotmail.com>
2023-06-26 12:59:41 -0500

    EtsyTest class shared

--- src/test/java/com/cydeo/tests/avengers/week2/EtsyTest.java
+++ src/test/java/com/cydeo/tests/avengers/week2/EtsyTest.java
@@ -1,8 +1,11 @@
 package com.cydeo.tests.avengers.week2;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -27,11 +30,23 @@ public class EtsyTest {
 
         //TC: Etsy checkbox and radio button
         //		1. Go to https://www.etsy.com
+        driver.get("https://www.etsy.com");
+
         //		2. Search for “wooden spoon”
+        WebElement searchBox = driver.findElement(By.cssSelector("input#global-enhancements-search-query"));
+        searchBox.sendKeys("wooden spoon");
+
+
+
         //		3. Click on filters
         //		4. Select free shipping, on sale
         //		5. Select under $25 Click on apply filters
         //		6. Print count of results
     }
 
+
+    @AfterMethod
+    public void teardown(){
+        driver.quit();
+    }
 }
