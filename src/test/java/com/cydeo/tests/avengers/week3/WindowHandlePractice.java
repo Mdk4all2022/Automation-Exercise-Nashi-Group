1bcad1266fa4426135577bb71862e634827edf3b
adamar <aysun114@hotmail.com>
2023-07-03 14:59:31 -0500

    WindowHandlePractice added

--- src/test/java/com/cydeo/tests/avengers/week3/WindowHandlePractice.java
+++ src/test/java/com/cydeo/tests/avengers/week3/WindowHandlePractice.java
@@ -1,12 +1,16 @@
 package com.cydeo.tests.avengers.week3;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
+import java.util.Set;
 
 public class WindowHandlePractice {
 
@@ -34,17 +38,47 @@ public class WindowHandlePractice {
     @Test
     public void window_handle_test(){
         //   - Navigate to "https://testpages.herokuapp.com/styled/windows-test.html"
+         driver.get("https://testpages.herokuapp.com/styled/windows-test.html");
+
+         // getWindowHandle() method gives current focus window id
+         String mainWindowhandle = driver.getWindowHandle();
+        System.out.println("mainWindowhandle = " + mainWindowhandle);
 
 
         //   - Click Basic Ajax in new page
-
+        driver.findElement(By.id("gobasicajax")).click();
 
         //   - Click Attributes in new page
-
+        driver.findElement(By.id("goattributes")).click();
 
         //   - Click Alerts In A New Window From JavaScript
+        driver.findElement(By.id("goalerts")).click();
+
+        // getWindowHandles() gives all windows handle id
+        // Since all window handles are unique, getWindowhandles() method return to the Set<String>
+        Set<String> allWindowHandles = driver.getWindowHandles();
+
+
         //   - Switch to "Element Attributes Examples" window
         //   - Assert header is Element Attributes Examples
+        for (String eachWindow : allWindowHandles) {
+
+            driver.switchTo().window(eachWindow);
+            System.out.println("driver.getTitle() = " + driver.getTitle());
+            System.out.println("driver.getWindowHandle() = " + driver.getWindowHandle());
+
+            if(driver.getTitle().contains("Attributes")){
+                WebElement header = driver.findElement(By.xpath("//h1[.='Element Attributes Examples']"));
+                Assert.assertTrue(header.getText().equals("Element Attributes Examples"));
+                break;
+            }
+        }
+
+
+
+
+
+
 
     }
 }
