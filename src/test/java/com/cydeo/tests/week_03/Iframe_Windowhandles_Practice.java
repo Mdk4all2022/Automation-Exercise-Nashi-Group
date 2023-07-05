5346fa31be00ce6499f290dc28b0e759bf80c4b4
adamar <aysun114@hotmail.com>
2023-07-05 10:11:09 -0500

    Iframe_Windowhandles_Practice added

---
+++ src/test/java/com/cydeo/tests/week_03/Iframe_Windowhandles_Practice.java
@@ -0,0 +1,45 @@
+package com.cydeo.tests.week_03;
+
+import com.cydeo.tests.base.TestBase;
+import org.testng.annotations.Test;
+
+public class Iframe_Windowhandles_Practice extends TestBase {
+
+    //Test website: http://www.w3schools.com/tags/tryit.asp?filename=tryhtml_link_target
+    //
+    //Test Scenario:
+    //
+    //Step 1- Go to the above URL.
+    //Step 2- Get the current window’s handle and write to the console window. It must be the first window handle.
+    //Step 3- Locate the “Visit W3Schools.com!” link and click it.
+    //Step 4- Get all window handles and hold them in a list.
+    //Step 5- Write to total window handle number to the console. It must be 2.
+    //Step 6- Switch to the second window.
+    //Step 7- Get the current window’s handle and write to the console window. It must be a second window handle.
+    //Step 8- Check the upper left side logo in the second window.
+    //Step 9- Go back (Switch) to the first window.
+    //Step 10- Get the current window’s handle and write to the console window. It must be the first window handle.
+    //Step 11- Check the See Run Button Text. It must contain “Run >” text.
+
+
+    @Test
+    public void iframe_windowhandle_test(){
+        //Step 1- Go to the above URL.
+
+
+        //Step 2- Get the current window’s handle and write to the console window. It must be the first window handle.
+
+
+        //Step 3- Locate the “Visit W3Schools.com!” link and click it.
+        //Step 4- Get all window handles and hold them in a list.
+        //Step 5- Write to total window handle number to the console. It must be 2.
+        //Step 6- Switch to the second window.
+        //Step 7- Get the current window’s handle and write to the console window. It must be a second window handle.
+        //Step 8- Check the upper left side logo in the second window.
+        //Step 9- Go back (Switch) to the first window.
+        //Step 10- Get the current window’s handle and write to the console window. It must be the first window handle.
+        //Step 11- Check the See Run Button Text. It must contain “Run >” text.
+
+    }
+
+}
