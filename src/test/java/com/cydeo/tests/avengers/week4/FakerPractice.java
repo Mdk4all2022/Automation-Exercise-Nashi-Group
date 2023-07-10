a2a247e6aca1f3f5c5abf437f9a1294eed9c3018
adamar <aysun114@hotmail.com>
2023-07-10 14:39:43 -0500

    FakerPractice added

--- src/test/java/com/cydeo/tests/avengers/week4/FakerPractice.java
+++ src/test/java/com/cydeo/tests/avengers/week4/FakerPractice.java
@@ -1,6 +1,10 @@
 package com.cydeo.tests.avengers.week4;
 
 import com.cydeo.tests.base.TestBase;
+import com.github.javafaker.Faker;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class FakerPractice extends TestBase {
@@ -9,17 +13,29 @@ public class FakerPractice extends TestBase {
     public void faker_test(){
 
         //        1. Launch browser
-
-
         //        2. Navigate to url 'https://automationexercise.com/login'
-
+         driver.get("https://automationexercise.com/login");
 
         //        3. Verify 'New User Signup!' is visible
-
+        WebElement newUserSignup = driver.findElement(By.xpath("//h2[.='New User Signup!']"));
+        Assert.assertTrue(newUserSignup.isDisplayed());
 
         //        4. Enter name and email address
+        Faker faker = new Faker();
+
+        WebElement name = driver.findElement(By.xpath("//input[@name='name']"));
+        name.sendKeys(faker.name().firstName());
+
+        WebElement email = driver.findElement(By.xpath("//input[@data-qa='signup-email']"));
+        email.sendKeys(faker.internet().emailAddress());
+
         //        5. Click 'Signup' button
+        driver.findElement(By.xpath("//button[.='Signup']")).click();
+
         //        6. Verify that 'ENTER ACCOUNT INFORMATION' is visible
+        WebElement header = driver.findElement(By.xpath("//h2[.='Enter Account Information']"));
+        Assert.assertTrue(header.isDisplayed());
+
         //        7. Fill details: Title, Name, Email, Password, Date of birth
         //        8. Select checkbox 'Sign up for our newsletter!'
         //        9. Select checkbox 'Receive special offers from our partners!'
