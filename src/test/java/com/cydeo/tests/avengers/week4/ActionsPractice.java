d515ee448c7958ed178a08160845369fed24e467
adamar <aysun114@hotmail.com>
2023-07-10 12:09:09 -0500

    ActionsPractice added

---
+++ src/test/java/com/cydeo/tests/avengers/week4/ActionsPractice.java
@@ -0,0 +1,24 @@
+package com.cydeo.tests.avengers.week4;
+
+import com.cydeo.tests.base.TestBase;
+import org.testng.annotations.Test;
+
+public class ActionsPractice extends TestBase {
+
+
+    @Test
+    public void actions_test(){
+
+        //    - Go to  "https://www.globalsqa.com/demo-site/draganddrop/"
+
+
+        //    - Drag and Drop "High Tatras" and "High Tatras 2" into "Trash"
+
+
+        //    - Verify Trash has 2 photo
+
+
+
+    }
+
+}
